ebfdf11872c4aa3748bb2ca3239dc7cb55a516ec
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-03 23:36:38 +0200

    Starts problem 15.

---
+++ src/main/java/net/euler/problem015/Problem.java
@@ -0,0 +1,19 @@
+package net.euler.problem015;
+
+/**
+ * Starting in the top left corner of a 2×2 grid, and only being able to move to
+ * the right and down, there are exactly 6 routes to the bottom right corner.
+ * 
+ * How many such routes are there through a 20×20 grid?
+ */
+public class Problem {
+	
+	public static void main(String[] args) {
+		System.out.println(routes(2));
+//		System.out.println(routes(20));
+	}
+
+	static long routes(int gridSize) {
+		return 0;
+	}
+}
---
+++ src/test/java/net/euler/problem015/ProblemTest.java
@@ -0,0 +1,15 @@
+package net.euler.problem015;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void testroutes() {
+		assertEquals(6, Problem.routes(2));
+//		assertEquals(, Problem.routes(20));
+	}
+
+}
