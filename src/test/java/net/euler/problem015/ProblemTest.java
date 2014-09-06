b024957f50a34571582dc15cbcc6e6351f4e71f6
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-06 21:34:24 +0200

    Fixes test.

--- src/test/java/net/euler/problem015/ProblemTest.java
+++ src/test/java/net/euler/problem015/ProblemTest.java
@@ -8,8 +8,8 @@ public class ProblemTest {
 
 	@Test
 	public void testroutes() {
-		assertEquals(6, Problem.routes(2));
-//		assertEquals(, Problem.routes(20));
+		assertEquals(6, Problem.calculatePaths(2));
+		assertEquals(137846528820l, Problem.calculatePaths(20));
 	}
 
 }
