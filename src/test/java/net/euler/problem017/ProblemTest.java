1d0b718090f4a08d8c5a3f5d01e5c709dcc4edc8
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-09 17:40:34 +0200

    Problem 17 test.

---
+++ src/test/java/net/euler/problem017/ProblemTest.java
@@ -0,0 +1,14 @@
+package net.euler.problem017;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void testCountLetters() {
+		assertEquals(21124, Problem.countLetters());
+	}
+
+}
