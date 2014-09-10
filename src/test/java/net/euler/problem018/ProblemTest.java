ee6d2a2d6fed8c4f8266b99cba1a13207602ceeb
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-10 23:43:10 +0200

    Problem 18 outline.

---
+++ src/test/java/net/euler/problem018/ProblemTest.java
@@ -0,0 +1,14 @@
+package net.euler.problem018;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void testTriangle() {
+		fail("Not yet implemented");
+	}
+
+}
