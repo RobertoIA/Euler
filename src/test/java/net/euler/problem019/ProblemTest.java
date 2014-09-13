c2d207c865e5f098937cee569405bc06c195d2f2
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-13 21:54:03 +0200

    Problem 19 outline.

---
+++ src/test/java/net/euler/problem019/ProblemTest.java
@@ -0,0 +1,14 @@
+package net.euler.problem019;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
