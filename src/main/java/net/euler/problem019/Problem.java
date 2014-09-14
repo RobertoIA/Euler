49649da337909e2af890047dd0763ee5b02f229a
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-14 18:50:45 +0200

    Calculates leap years.

--- src/main/java/net/euler/problem019/Problem.java
+++ src/main/java/net/euler/problem019/Problem.java
@@ -4,13 +4,12 @@ package net.euler.problem019;
  * You are given the following information, but you may prefer to do some
  * research for yourself.
  * 
- * 1 Jan 1900 was a Monday.
- * Thirty days has September, April, June and November.
+ * 1 Jan 1900 was a Monday. Thirty days has September, April, June and November.
  * All the rest have thirty-one, Saving February alone, Which has twenty-eight,
- * rain or shine.
- * And on leap years, twenty-nine.
- * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
- * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ * rain or shine. And on leap years, twenty-nine. A leap year occurs on any year
+ * evenly divisible by 4, but not on a century unless it is divisible by 400.
+ * How many Sundays fell on the first of the month during the twentieth century
+ * (1 Jan 1901 to 31 Dec 2000)?
  */
 public class Problem {
 
@@ -19,4 +18,13 @@ public class Problem {
 
 	}
 
+	static boolean isLeapYear(int year) {
+		if (year % 4 != 0)
+			return false;
+		if (year % 100 != 0)
+			return true;
+		if (year % 400 != 0)
+			return false;
+		return true;
+	}
 }
--- src/test/java/net/euler/problem019/ProblemTest.java
+++ src/test/java/net/euler/problem019/ProblemTest.java
@@ -7,6 +7,13 @@ import org.junit.Test;
 public class ProblemTest {
 
 	@Test
+	public void testIsLeapYear() {
+		assertTrue(Problem.isLeapYear(2016));
+		assertTrue(Problem.isLeapYear(2008));
+		assertFalse(Problem.isLeapYear(2013));
+	}
+	
+	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
