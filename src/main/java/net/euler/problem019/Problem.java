d18df74dcc5f67d5e3bb3a121a6a72c860b190a8
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-09-15 22:13:14 +0200

    Outlines solution to problem 19.

--- src/main/java/net/euler/problem019/Problem.java
+++ src/main/java/net/euler/problem019/Problem.java
@@ -13,18 +13,59 @@ package net.euler.problem019;
  */
 public class Problem {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	enum Months {
+		JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
 	}
 
-	static boolean isLeapYear(int year) {
-		if (year % 4 != 0)
+	enum Weekdays {
+		MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+	}
+
+	public static void main(String[] args) {
+		Problem problem = new Problem();
+		System.out.println(problem.countFirstSundays(1900, 2000));
+	}
+
+	int countFirstSundays(int firstYear, int lastYear) {
+		Month currMonth = new Month(Months.JANUARY, Weekdays.MONDAY, firstYear);
+		int count = 0;
+		
+		while (currMonth.year <= lastYear) {
+			if(currMonth.isSundayFirst()) count++;
+			currMonth = currMonth.getNextMonth();
+		}
+
+		return count;
+	}
+
+	class Month {
+
+		Months month;
+		Weekdays firstDay;
+		int year;
+
+		Month(Months month, Weekdays firstDay, int year) {
+			this.month = month;
+			this.firstDay = firstDay;
+			this.year = year;
+		}
+
+		Month getNextMonth() {
+			return this;
+		}
+
+		boolean isSundayFirst() {
 			return false;
-		if (year % 100 != 0)
+		}
+
+		private boolean isLeapYear(int year) {
+			if (year % 4 != 0)
+				return false;
+			if (year % 100 != 0)
+				return true;
+			if (year % 400 != 0)
+				return false;
 			return true;
-		if (year % 400 != 0)
-			return false;
-		return true;
+		}
 	}
 }
