06c0a01e0e80dbc0a6118ffe94168512bcf25e66
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-10-06 18:13:04 +0200

    Problem 24.

--- src/main/java/net/euler/problem024/Problem.java
+++ src/main/java/net/euler/problem024/Problem.java
@@ -1,5 +1,7 @@
 package net.euler.problem024;
 
+import java.util.Arrays;
+
 /**
  * A permutation is an ordered arrangement of objects. For example, 3124 is one
  * possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
@@ -14,27 +16,50 @@ package net.euler.problem024;
 public class Problem {
 
 	public static void main(String[] args) {
-		System.out.println(getNPermutation(1, new int[]{0, 1, 2}));
-		System.out.println(getNPermutation(2, new int[]{0, 1, 2}));
-		System.out.println(getNPermutation(3, new int[]{0, 1, 2}));
-		System.out.println(getNPermutation(4, new int[]{0, 1, 2}));
-		System.out.println(getNPermutation(5, new int[]{0, 1, 2}));
-		System.out.println(getNPermutation(6, new int[]{0, 1, 2}));
+		System.out.println(Arrays.toString(Problem.getNPermutation(1_000_000, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})));
 	}
 
-	static int getNPermutation(int n, int[] digits) {
-		int permutation = 0;
-		
-		for(int i = 0; i < digits.length; i++) {
-			permutation += 1 * (digits.length - i - 1);
+	static int[] getNPermutation(int n, int[] digits) {
+		int[] permutation = new int[digits.length];
+		Arrays.sort(digits);
+
+		int factorial;
+		int pos = n;
+		int index;
+		for (int i = permutation.length - 1; i >= 0; i--) {
+			factorial = factorial(i);
+			index = 0;
+			while (pos > factorial) {
+				pos -= factorial;
+				index++;
+			}
+			permutation[permutation.length - 1 - i] = digits[index];
+			digits = delete(index, digits);
 		}
-		
+
 		return permutation;
 	}
 	
+	static int[] delete(int index, int[] array) {
+		if(index < 0 || index >= array.length) return array;
+		
+		int[] newArray = new int[array.length - 1];
+		int pos = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(i != index) {
+				newArray[pos] = array[i];
+				pos++;
+			}
+		}
+		
+		
+		return newArray;
+	}
+
 	static int factorial(int n) {
 		int factorial = 1;
-		while(n > 1) factorial *= n--;
+		while (n > 1)
+			factorial *= n--;
 		return factorial;
 	}
 }
--- src/test/java/net/euler/problem024/ProblemTest.java
+++ src/test/java/net/euler/problem024/ProblemTest.java
@@ -15,6 +15,6 @@ public class ProblemTest {
 	@Test
 	public void testGetNPermutation() {
 		assertArrayEquals(new int[]{1, 2, 0}, Problem.getNPermutation(4, new int[]{0, 1, 2}));
-//		assertArrayEquals(new int[]{}, Problem.getNPermutation(1_000_000, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+		assertArrayEquals(new int[]{2, 7, 8, 3, 9, 1, 5, 4, 6, 0}, Problem.getNPermutation(1_000_000, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
 	}
 }
